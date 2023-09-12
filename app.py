f6b61b479fcdd417af13042c9774d9b8ecaebf0f
Amy <devennyamy@gmail.com>
2023-09-11 20:30:22 -0400

    completed question functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template
-from surveys import satisfaction_survey
+from flask import Flask, request, render_template, redirect
+from surveys import Survey, satisfaction_survey
 
 
 # create a Flask application object named app
@@ -11,20 +11,41 @@ app.config['SECRET_KEY'] = "I love kitties"
 # empty list to capture survey answers
 responses = []
 
+# survey title and instructions for use throughtout the app
+title = satisfaction_survey.title
+instructions = satisfaction_survey.instructions
+
 # root page
 @app.route('/')
 def home_page():
-  return render_template('index.html')
+  return render_template('index.html', title=title, instructions=instructions)
 
 @app.route('/questions/<int:q_id>')
 def question_page(q_id):
-  # question = satisfaction_survey.questions[q_id]
+  # empty list for the questions from the survey object 
+  questions = []
+
+  # loop through the questions in the survey and add each one to the questions list, making them iterable
   for question in satisfaction_survey.questions:
-    if 0 <= q_id < len(satisfaction_survey.questions):
-      q_n = question.question
+    questions.append(question.question)
+
+  # this is the number to display in the h5 in the question page, one more than the index of the question
+  q_num = q_id + 1
+
+  # question is the item in the questions list at the index of q_id in the url (this question being asked on the page with index corresponding to url(q_id))
+  # question_number is the number to display on the page asking the question
+  return render_template('question.html', title=title, question=questions[q_id], question_number = q_num)
+
+
+  
+  # for i, question in satisfaction_survey.questions:
+  #   print(satisfaction_survey.questions)
+  #   if 0 <= q_id < len(satisfaction_survey.questions):
+  #     q_n = question.question
+      
       # question_number = q_id + 1
-      question_number = satisfaction_survey.questions[q_id]
-      return render_template('question.html', question=q_n, question_number = question_number)
+      # # question_number = satisfaction_survey.questions[q_id]
+      # 
   
 
 # I want to loop through the questions and assign each index to it's corresponding number in the url
