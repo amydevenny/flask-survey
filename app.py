46547ab6564a1b0c9e2fa5b815924b38e5ab9094
Amy <devennyamy@gmail.com>
2023-09-14 11:25:18 -0400

    flask survey working

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
-from flask import Flask, request, render_template, redirect
-from surveys import Survey, satisfaction_survey
+from flask import Flask, request, render_template, redirect, flash, session
+# from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
 
 
 # create a Flask application object named app
 app = Flask(__name__)
 
+
 # needed for the debugger 
-app.config['SECRET_KEY'] = "I love kitties"
+# app.config['SECRET_KEY'] = "I love kitties"
+# debug = DebugToolbarExtension(app)
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
 
 # empty list to capture survey answers
 responses = []
 
-# survey title and instructions for use throughtout the app
-title = satisfaction_survey.title
-instructions = satisfaction_survey.instructions
-
 # root page
 @app.route('/')
 def home_page():
-  return render_template('index.html', title=title, instructions=instructions)
+  return render_template('index.html', survey=survey)
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Save response and redirect to next question."""
+
+    # get the response choice
+    get_choice = request.form['answer']
+
+    # add this response to the session
+    responses.append(get_choice)
+    print(responses)
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/thank-you")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
 
 @app.route('/questions/<int:q_id>')
 def question_page(q_id):
@@ -26,43 +47,40 @@ def question_page(q_id):
   questions = []
 
   # loop through the questions in the survey and add each one to the questions list, making them iterable
-  for question in satisfaction_survey.questions:
+  for question in survey.questions:
     questions.append(question.question)
+
+  # get the number of questions to redirect after all questions are asked
+  num_questions = len(questions)
+  print(num_questions)
 
   # this is the number to display in the h5 in the question page, one more than the index of the question
   q_num = q_id + 1
-
-  # question is the item in the questions list at the index of q_id in the url (this question being asked on the page with index corresponding to url(q_id))
-  # question_number is the number to display on the page asking the question
-  return render_template('question.html', title=title, question=questions[q_id], question_number = q_num)
-
-
   
-  # for i, question in satisfaction_survey.questions:
-  #   print(satisfaction_survey.questions)
-  #   if 0 <= q_id < len(satisfaction_survey.questions):
-  #     q_n = question.question
-      
-      # question_number = q_id + 1
-      # # question_number = satisfaction_survey.questions[q_id]
-      # 
+  # the question in the item in the questions list at the index of the q_id in the url
+  question = survey.questions[q_id]
+
+  if (responses is None):
+     return redirect('/')
   
+  if (len(responses) != q_id):
+     flash(f'Please answer the questions in order')
+     return redirect(f'/questions/{len(responses)}')
+     
+  # if the question number is less than or equal to the amount of questions, render the question template
+  if (q_num <= num_questions):
+    # question is the item in the questions list at the index of q_id in the url (the question being asked on the page with index corresponding to url(q_id))
+    # question_number is the number to display on the page asking the question
+    return render_template('question.html', question_text=questions[q_id], question_number = q_num, question=question, survey=survey)
+  
+  # if q_num is not a valid question index or the thank you page conditional, go to error page 
+  else:
+    return redirect('/404')
+  
+@app.route('/404')
+def error_page():
+  return render_template('404.html')
 
-# I want to loop through the questions and assign each index to it's corresponding number in the url
-# or do I make a dictionary from the list?
-
-
-
-
-
-
-
-
-# to print all questions in a survey
-# for question in satisfaction_survey.questions:
-    # ...:     print(question.question)
-
-
-# To print each question
-# for i, question in enumerate(satisfaction_survey.questions):
-#     ...:         print(f"Question {i+1}: {question}")
+@app.route('/thank-you')
+def thank_you_page():
+  return render_template('thank-you.html', responses=responses)
